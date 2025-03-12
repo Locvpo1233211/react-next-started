83f9eeefe9d20784cc3a4cb1bcb504ec2731137e
Locvpo1233211 <trongnghia1112223333@gmail.com>
2025-03-12 18:59:07 +0700

    react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import "./index.css";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/user",
+        element: <div>user aaa</div>,
+    },
+    {
+        path: "/tracks",
+        element: <div>tracks aaa</div>,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
